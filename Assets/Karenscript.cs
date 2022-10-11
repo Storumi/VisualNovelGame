bce36027dc4fb26ff9a1caf9bce6ff0b20a981f2
Storumi <nas642@nyu.edu>
2022-10-11 15:48:54 -0400

    objects

--- Assets/Karenscript.cs
+++ Assets/Karenscript.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Karenscript : MonoBehaviour
 {
+    public TMP_Text Dialogue;
+    public TMP_Text InputRequest;
+    public int stage = 0;
     // Start is called before the first frame update
     void Start()
     {
